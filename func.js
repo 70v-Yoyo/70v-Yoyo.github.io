3c9a6a8cc03aaa9f12f33d6e5d5ec0b10e93f919
70v-Yoyo <736037589@qq.com>
2025-02-11 23:14:04 +0800

    Site updated: 2025-02-11 23:14:03

--- func.js
+++ func.js
@@ -100,7 +100,7 @@ function getRandomColor() {
 	  daytip.classList.add('tooltip');
 	  together.appendChild(daytip);
 	  calendarElement.innerHTML = ''; // 清空之前的内容
-	  
+	  month_text=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 	  for (let month = 0; month < 12; month++) {
 		const monthContainer = document.createElement('div');
 		monthContainer.classList.add('month-container');//添加一个新的类名
@@ -108,7 +108,7 @@ function getRandomColor() {
 			// 添加月份头部
 			const monthHeader = document.createElement('div');
 			monthHeader.classList.add('month-header');
-			monthHeader.textContent = `${month + 1}月`;
+			monthHeader.textContent = month_text[month];
 			month_float.appendChild(monthHeader);
 		 }
 		  
@@ -249,7 +249,7 @@ function getRandomColor() {
 
   function all(){
 	//这里插入checkedDays变量
-	checkedDays = ["2025-02-10","2025-02-11","2025-02-11","2025-02-08"];
+	checkedDays = ["2025-02-10","2025-02-08","2025-02-11","2025-02-11"];
 	currentYear=new Date().getFullYear();
 	//默认今年
 	get_elements();
